f3297c284e37b69c32ec4234b4282f22d6b1b602
zl25-drexel <zl25-drexel@eab46383-0c4a-0410-80f0-95ac5680b747>
2008-04-11 00:24:00 +0000

    1.5.4 bug?

--- jgeocoder-import/src/main/groovy/hello.groovy
+++ jgeocoder-import/src/main/groovy/hello.groovy
@@ -4,7 +4,34 @@ import TigerDefinition
 import groovy.sql.Sql
 import org.apache.commons.lang.StringUtils
 
+def cleanup = true
+
+def db = Sql.newInstance("jdbc:hsqldb:file:/home/jliang/Desktop/hsqldb/testdb;shutdown=true", 
+        "sa", "", 'org.hsqldb.jdbcDriver')
+
+File f = new File(/C:\Users\jliang\workspace\jgeocoder-import\src\test.RT1/)
+if(cleanup)
+  cleanUp(db)
+  
+init(db)
+processFile(db, f)
+
+db.close()
+
+void cleanUp(def db){
+  TigerDefinition.TIGER_TABLES.each{
+    try{ db.execute('drop table ' +it.name)}catch(Exception e){ println e.message }
+  }  
+}
+
+void init(def db){
+  TigerDefinition.TIGER_TABLES.each{
+    try{ db.execute(it.ddl)}catch(Exception e){ println e.message }
+  }
+}
+
 void processFile(def db, File f){
+
   def ext = f.getName().substring(f.getName().lastIndexOf('.')+1)
   if(ext == 'MET') return
   def tableName = 'tiger_'+ext.toLowerCase().substring(2)
@@ -21,7 +48,6 @@ void processFile(def db, File f){
     q << '?'
   }
   def insert = "insert into ${table.name} (${col.join(',')}) values (${q.join(',')});"
-  
   f.eachLine{line ->
     if(StringUtils.isBlank(line)) return
     
@@ -31,43 +57,11 @@ void processFile(def db, File f){
       if(val == ''){colval << null}
       else{
         if(val.startsWith('+')) val = val.substring(1)
-        colval << (c.isNum ? Integer.parseInt(val) : val)
+        colval << (c.isNum ? Long.parseLong(val) : val)
       }
     }
-    println colval
     db.execute(insert, colval)
   }
   
 }
 
-def db = Sql.newInstance("jdbc:hsqldb:file:/home/jliang/Desktop/hsqldb/testdb;shutdown=true", 
-        "sa", "", 'org.hsqldb.jdbcDriver')
-
-TigerDefinition.TIGER_TABLES.each{
-   try{ db.execute(it.ddl)}catch(Exception e){ println e.message }
-}
-
-File f = new File('/home/jliang/Desktop/TGR42001/test.RT1')
-processFile(db, f)
-
-//
-//
-//String athleteInsert = '''
-//    INSERT INTO Athlete (firstname, lastname, dateOfBirth)
-//                VALUES (?, ?, ?);
-//'''
-//db.execute athleteInsert, ['Paul',    'Tergat',     '1969-06-17']
-//db.execute athleteInsert, ['Khalid', 'Khannouchi', '1971-12-22']
-//db.execute athleteInsert, ['Ronaldo', 'da Costa',   '1970-06-07']
-//
-//println ' Athlete Info '.center(25,'-')
-//def fmt = new java.text.SimpleDateFormat('dd. MMM yyyy (E)',
-//                                         Locale.US)
-//db.eachRow('SELECT * FROM Athlete'){ athlete ->
-//    println athlete.firstname + ' ' + athlete.lastname
-//    println 'born on '+ fmt.format(athlete.dateOfBirth)
-//    println '-' * 25
-//}
-
-
-db.close()
