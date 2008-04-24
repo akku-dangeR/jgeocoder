c0f1544a4e95e6ffdad45b975c7b481044af5b6a
zl25-drexel <zl25-drexel@eab46383-0c4a-0410-80f0-95ac5680b747>
2008-04-24 18:40:07 +0000

    Removed file/folder

--- jgeocoder/src/test/groovy/net/sourceforge/jgeocoder/us/GroovyTest.groovy
+++
@@ -1,18 +0,0 @@
-package net.sourceforge.jgeocoder.us
-
-import net.sourceforge.jgeocoder.us.RegexLibraryimport java.util.concurrent.ExecutorServiceimport java.util.concurrent.Executorsimport java.util.concurrent.Futureimport java.math.BigIntegerimport java.math.BigInteger
-import java.util.regex.Patternimport java.util.regex.Matcher
-class Person{
-  String name
-  int age
-}
-
-class GroovyTest extends GroovyTestCase {
-    def testClosure(Closure c){
-      c(1,2 ,3,4,5)
-    }
-	void testGroovy() {
-	  def t = {a, b,c,d,e -> println a+b}
-	  println testClosure(t)
-	}
-}
