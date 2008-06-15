faf197e0f4621dab3eb8b7ec40bba1aaf93716e8
zl25-drexel <zl25-drexel@eab46383-0c4a-0410-80f0-95ac5680b747>
2008-06-15 14:14:19 +0000

    fixed url

--- jgeocoder/src/main/java/net/sourceforge/jgeocoder/tiger/H2DbDataSourceFactory.java
+++ jgeocoder/src/main/java/net/sourceforge/jgeocoder/tiger/H2DbDataSourceFactory.java
@@ -5,11 +5,15 @@ import javax.sql.DataSource;
 import net.sourceforge.jgeocoder.CommonUtils;
 
 import org.h2.jdbcx.JdbcDataSource;
-
+/**
+ * TODO javadocs me
+ * @author jliang
+ *
+ */
 public class H2DbDataSourceFactory{
   private H2DbDataSourceFactory(){}
   private static String _tigerUrl =
-    CommonUtils.nvl(System.getProperty("jgeocoder.tiger.url"), "/usr/local/jgeocoder/tiger/tiger");
+    CommonUtils.nvl(System.getProperty("jgeocoder.tiger.url"), "jdbc:h2:/usr/local/jgeocoder/tiger/tiger;LOG=0");
 
   public static DataSource getH2DbDataSource(){
     return getH2DbDataSource(_tigerUrl);
