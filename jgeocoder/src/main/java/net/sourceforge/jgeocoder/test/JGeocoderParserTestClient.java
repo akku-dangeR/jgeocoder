026ee8cd2ec3720fdff1d5b62ddd92a9e7831ea7
zl25-drexel <zl25-drexel@eab46383-0c4a-0410-80f0-95ac5680b747>
2008-06-15 14:12:25 +0000

    new test client

--- jgeocoder/src/main/java/net/sourceforge/jgeocoder/test/JGeocoderParserTestClient.java
+++ jgeocoder/src/main/java/net/sourceforge/jgeocoder/test/JGeocoderParserTestClient.java
@@ -1,11 +1,11 @@
 package net.sourceforge.jgeocoder.test;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
-import net.sourceforge.jgeocoder.AddressComponent;
+import net.sourceforge.jgeocoder.CommonUtils;
+import net.sourceforge.jgeocoder.JGeocodeAddress;
 import net.sourceforge.jgeocoder.tiger.JGeocoder;
-import net.sourceforge.jgeocoder.us.AddressParser;
 import net.sourceforge.jgeocoder.us.AddressStandardizer;
 
 
@@ -15,19 +15,14 @@ public class JGeocoderParserTestClient {
     JGeocoder jg = new JGeocoder();
     if(args.length != 0){
       in = args[0];
-      System.out.println("Parsing input...");
-      Map<AddressComponent, String> m  = AddressParser.parseAddress(in);
-      if(m == null){
-        System.out.println("Unable to parse input, please try again");
-        return;
-      }
-      printMap(m);
-      
-      System.out.println("Normalizing parsed address...");
-      m = AddressStandardizer.normalizeParsedAddress(m);
-      printMap(m);
-      System.out.println("Geocoding normalized address (Only zip and {city, state} geocoding are supported currently)...");
-      printMap(jg.geocode(m));
+      JGeocodeAddress ret  = jg.geocodeAddress(in);
+      long start = System.currentTimeMillis();
+      System.out.println();
+      System.out.println("Parsed: "+ CommonUtils.nvl(AddressStandardizer.toSingleLine(ret.getParsedAddr()), "null"));
+      System.out.println("Normalized: "+ CommonUtils.nvl(AddressStandardizer.toSingleLine(ret.getNormalizedAddr()), "null"));
+      System.out.println("Geocoded: "+ CommonUtils.nvl(AddressStandardizer.toSingleLine(ret.getGeocodedAddr()), "null"));
+      System.out.println(ret);
+      CommonUtils.printElapsed(start, TimeUnit.SECONDS);
       jg.cleanup();
       return;
     }
@@ -39,29 +34,17 @@ public class JGeocoderParserTestClient {
     BufferedReader inbuf = new BufferedReader(new InputStreamReader(System.in));
     
     while ((in = inbuf.readLine()) != null && in.length() != 0){
-      System.out.println("Parsing input...");
-      Map<AddressComponent, String> m  = AddressParser.parseAddress(in);
-      if(m == null){
-        System.out.println("Unable to parse input, please try again");
-        continue;
-      }
-      printMap(m);
-      
-      System.out.println("Normalizing parsed address...");
-      m = AddressStandardizer.normalizeParsedAddress(m);
-      printMap(m);
-      System.out.println("Geocoding normalized address (Only zip and {city, state} geocoding are supported currently)...");
-      printMap(jg.geocode(m));
+      long start = System.currentTimeMillis();
+      System.out.println();
+      JGeocodeAddress ret  = jg.geocodeAddress(in);
+      System.out.println("Parsed: "+ CommonUtils.nvl(AddressStandardizer.toSingleLine(ret.getParsedAddr()), "null"));
+      System.out.println("Normalized: "+ CommonUtils.nvl(AddressStandardizer.toSingleLine(ret.getNormalizedAddr()), "null"));
+      System.out.println("Geocoded: "+ CommonUtils.nvl(AddressStandardizer.toSingleLine(ret.getGeocodedAddr()), "null"));
+      System.out.println(ret);
+      CommonUtils.printElapsed(start, TimeUnit.SECONDS);
+      System.out.println();
     }
     jg.cleanup();
   }
-  
-  private static void printMap(Map<AddressComponent, String> m){
-    System.out.println();
-    System.out.println("Address: "+AddressStandardizer.toSingleLine(m));
-    for(Map.Entry<AddressComponent, String> e : m.entrySet()){
-      System.out.println(e.getKey()+" : "+e.getValue());
-    }
-    System.out.println();
-  }
+
 }
