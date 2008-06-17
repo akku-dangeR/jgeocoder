84cb44fa3506e2f7f9046c9ba1a23f726e31f6c1
zl25-drexel <zl25-drexel@eab46383-0c4a-0410-80f0-95ac5680b747>
2008-06-17 23:16:01 +0000

    test

--- jgeocoder/src/main/java/net/sourceforge/jgeocoder/test/JaysTest.java
+++ jgeocoder/src/main/java/net/sourceforge/jgeocoder/test/JaysTest.java
@@ -2,6 +2,9 @@ package net.sourceforge.jgeocoder.test;
 
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.lang.builder.ToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+
 import net.sourceforge.jgeocoder.CommonUtils;
 import net.sourceforge.jgeocoder.JGeocodeAddress;
 import net.sourceforge.jgeocoder.tiger.H2DbDataSourceFactory;
@@ -12,12 +15,12 @@ public class JaysTest{
   public static void main(String[] args) {
     JGeocoderConfig config = new JGeocoderConfig();
     config.setJgeocoderDataHome("C:\\Users\\jliang\\Desktop\\jgeocoder\\data");
-    config.setTigerDataSource(H2DbDataSourceFactory.getH2DbDataSource("jdbc:h2:C:\\Users\\jliang\\Desktop\\jgeocoder\\tiger\\tiger;LOG=0"));
+    config.setTigerDataSource(H2DbDataSourceFactory.getH2DbDataSource("jdbc:h2:C:\\Users\\jliang\\Desktop\\jgeocoder\\tiger\\tiger;LOG=0;UNDO_LOG=0"));
     JGeocoder jg = new JGeocoder(config);
     long start = System.currentTimeMillis();
 //    for(int i =0; i<100; i++){
       JGeocodeAddress addr = jg.geocodeAddress("lazaros pizza house 1743 south st philadelphia pa 19146");
-      System.out.println(addr);
+      System.out.println(ToStringBuilder.reflectionToString(addr, ToStringStyle.MULTI_LINE_STYLE));
 //    }
     CommonUtils.printElapsed(start, TimeUnit.SECONDS);
     
