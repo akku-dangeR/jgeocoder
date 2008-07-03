e25db7afe8a790c90ac832e0be825b1c6df41d7d
zl25-drexel <zl25-drexel@eab46383-0c4a-0410-80f0-95ac5680b747>
2008-07-03 00:06:45 +0000

    [maven-release-plugin]  copy for tag jgeocoder-0.4

---
+++ tags/jgeocoder-0.4/src/main/java/net/sourceforge/jgeocoder/AddressComponent.java
@@ -0,0 +1,13 @@
+package net.sourceforge.jgeocoder;
+/**
+ * TODO javadocs me
+ * @author jliang
+ *
+ */
+public enum AddressComponent{
+    STREET, NAME, PREDIR, POSTDIR, TYPE, NUMBER, CITY, STATE, ZIP, LINE2, 
+    //intersections
+    STREET2, PREDIR2, POSTDIR2, TYPE2, 
+    //geocode populated
+    LAT, LON, COUNTY, TLID
+}
