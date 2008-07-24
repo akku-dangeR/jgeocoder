2db6571b968b92e5aba8ef6595945a34e718dfeb
zl25-drexel <zl25-drexel@eab46383-0c4a-0410-80f0-95ac5680b747>
2008-07-24 12:22:20 +0000

    data files generation script

--- jgeocoder-import/src/main/groovy/Config.groovy
+++ jgeocoder-import/src/main/groovy/Config.groovy
@@ -2,7 +2,7 @@
 class Config{
   def cleanup = true  //this drops everything and rebuilds
   def driverClass = 'org.h2.Driver'
-  def connectionString = /jdbc:h2:C:\Users\jliang\Desktop\jgeocoder\tiger\tiger;LOG=0;UNDO_LOG=0/
-  def tigerDataFiles = /C:\Users\jliang\Desktop\tiger\PA/
+  def connectionString = /jdbc:h2:C:\Users\jliang\Desktop\jgeocoder\tiger\ca;LOG=0;UNDO_LOG=0/
+  def tigerDataFiles = /C:\Users\jliang\Desktop\tiger\CA/
   def db = null
 }
